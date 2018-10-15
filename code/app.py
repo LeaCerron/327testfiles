d3d41f65300ce3e8a775bc104a5c54c85116fc9d
Lea Cerron <15lc50@queensu.ca>
2018-10-15 17:01:33 -0400

    changes to cancel tickets

--- code/app.py
+++ code/app.py
@@ -4,27 +4,39 @@ servicenumbers = ["12345",]
 cancelcount = 0
 serviceDeleteCount = {}
 
-def cancelticket(account, cancelaccount):
+def cancelTicket(account, cancelaccount, cancelcounter, cancelTicketCount):
     if cancelaccount not in servicenumbers:
-        return(0,"invalid service number")
-    print("please enter the number ot tickets to delet: ")
-    tickets = input() #enter number of tickets
-    
-    if(account == 'agent'):
-        if(cancelaccount in serviceDeleteCount):
-            if(tickets + serviceDeleteCount[cancelaccount] > 10):
-                return(0,"Deleteing too many tickets")
+        print("Service number not in the valid services list")
+        return(0)
+    print("Please enter the number ot tickets to delet: ")
+    try:
+        tickets = int(input()) #enter number of tickets
+    except:
+        tickets = 0
+    while(tickets == 0):
+        try:
+            tickets = int(input("Invalid input please try again: ")) 
+        except:
+            pass         
+    #check number of tickets
+    if (tickets < 1) | (tickets > 1000):
+        print("Number of tickets should be between 1 and 1000")
+        return(0)
+    if (account == 'agent'):
+        if(cancelaccount in cancelTicketCount):
+            if(tickets + cancelTicketCount[cancelaccount] > 10):
+                print("Agents are only allowed to delete 10 tickets per service per session")
+                return(0)
             else:
-                serviceDeleteCount[cancelaccount] = tickets
-        if(int(tickets) + cancelcount > 20):
-            return(0,"Deleting too many tickets")
-    if((int(tickets) < 1) | (int(tickets) > 1000)):
-        return(0,"invalid number of tickets")
+                cancelcounter += tickets
+        if(tickets + cancelcount > 20):
+            print("Agents are only allowed to delete 20 tickets per session")
+            return(0)
     today = datetime.now()
     endoffile("DEL " + cancelaccount + " " + tickets + " 00000 **** " + today.strftime("%Y%m%d"))
     return(tickets)
 
-def sellticket(account, sellaccount):
+def sellTicket(account, sellaccount):
     if sellaccount not in servicenumbers:
         return(0,"invalid service number")
     print("please enter the number ot tickets to sell: ")
@@ -42,6 +54,6 @@ def endoffile(string):
 def main():
     string = input()
     number = input()
-    cancelticket(string,number)
+    cancelTicket(string,number)
 
 main()
