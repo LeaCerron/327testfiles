06e5dab925e72126a10ce94b0f5d712ec3f37919
xXLinuxGodXx <connorway97@gmail.com>
2018-10-15 17:17:36 -0400

    Update deleteService.py

--- code/deleteService.py
+++ code/deleteService.py
@@ -1,24 +1,24 @@
 def deleteService(account, serviceList):
     if(account == "agent"):
         print("agent cannot delete services")
-        return 0;
+        return
     print("Enter the service number to be deleted")
     service = input()
     if not(service in serviceList):
         print("invalid service number")
-        return 0
+        return
     elif (newNumber[:1] = "0" | not(newNumber.len = 5):
         print("invalid service number")
-        return 0
+        return
     
     print("Enter the name of the service to be deleted")
     name = input()
     elif(name.len > 39 | name.len < 3):
         print("invalid service name")
-        return 0
+        return
     elif(name[:1] = " " | name[:-1] = " "):
         print("invalid service name")
-        return 0
+        return
     
     addToTransactions("DEL " + service + " 0 00000 " + name + " " + "0")
-    return 1:
+    return service
