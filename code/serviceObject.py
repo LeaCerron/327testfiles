f6dd12d0dafe389541a7cac6d90a7aff258f7178
Lea Cerron <15lc50@queensu.ca>
2018-11-07 16:04:29 -0500

    changed the service class and added examples

--- code/serviceObject.py
+++ code/serviceObject.py
@@ -1,8 +1,7 @@
 services = []
-#nameing convention for service objects is service# where # is the service # of an object
-#eval('service' + str(serviceNum))
 class Service:
-    def __init__(self, ticketsSold, serviceName, serviceDate):
+    def __init__(self, serviceNumber, ticketsSold, serviceName, serviceDate):
+        self.serviceNumber = serviceNumber
         self.serviceCapacity = 30
         self.ticketsSold = ticketsSold
         self.serviceName = serviceName
@@ -19,4 +18,11 @@ def changeTickets(service1, service2, tickets):
     service2.sellTickes(tickets)
 
 def main():
-    services.append = eval('service' + str(10000))
+    services.append(Service(10000, 0,'kingston', 12122020))
+    services[0].sellTickets(3)
+    print(services[0].serviceNumber)
+    print(services[0].ticketsSold)
+    print(services[0].serviceName)
+    print(services[0].serviceDate)
+
+main()
