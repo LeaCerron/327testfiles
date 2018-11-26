1d3d771238af6664bac166205a465cc6588c2e6d
TaraCarette <terra.the.great@gmail.com>
2018-11-26 17:08:09 -0500

    daily run script

---
+++ dailyRun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#times run frontend per day
+RUNS=3
+
+#clear out any old runs
+rm actualoutput/*
+
+#remove the old merged transaction file
+rm mergedTransactions.txt
+
+#run the frontend for the day
+for (( i = 1; i <= $RUNS; i++ )); do
+    python3 code/frontEnd.py serviceList.txt actualoutput/${i}.txt
+done
+
+#concatenate all output files to a merged transaction file
+for file in actualoutput/*; do
+    cat $file >> mergedTransactions.txt
+done
+
